fb9a24cc8d2ed0c01dfecf5951a078663475ce8c
andy <andy@andydote.co.uk>
2013-10-16 13:55:29 +0100

    model: add clock

--- model.lua
+++ model.lua
@@ -16,6 +16,16 @@ local model = {
 		this.buttons.zoomIn = MinimapZoomIn
 		this.buttons.zoomOut = MinimapZoomOut
 
+		local clock = TimeManagerClockButton
+		local clockBorder, clockText, clockAlarmTexture = clock:GetRegions()
+
+		this.clock = {
+			button = clock,
+			border = clockBorder,
+			text = clockText,
+			alarm = clockAlarmTexture,
+		}
+
 		return this
 
 	end,

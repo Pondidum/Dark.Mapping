f4f7a8ffbae3b80b785ed7b08e5612218cf24466
andy <andy@andydote.co.uk>
2013-10-18 22:21:01 +0100

    styleMail: style mail button

---
+++ features/styleMail.lua
@@ -0,0 +1,13 @@
+local addon, ns = ...
+
+ns.features.styleMail = function(model)
+
+	local mail = model.icons.mail
+
+	mail:SetPoint("TOPRIGHT", map, "BOTTOMRIGHT", 0, -ns.config.spacing)
+	mail:SetSize(15, 15)
+
+	style.addShadow(mail)
+	style.addBackground(mail)
+
+end

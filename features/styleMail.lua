ec49b5433924b2cc61bba64370ba0bcafe80aba1
andy <andy@stormbase.net>
2013-10-20 18:31:22 +0100

    styleMail: update mail icon saturation on new mail

--- features/styleMail.lua
+++ features/styleMail.lua
@@ -2,6 +2,7 @@ local addon, ns = ...
 
 local core = Dark.core
 local style = core.style
+local events = core.events.new()
 
 ns.features.styleMail = function(model)
 
@@ -11,6 +12,10 @@ ns.features.styleMail = function(model)
 	container:SetPoint("TOPRIGHT", map, "BOTTOMRIGHT", 0, -ns.config.spacing)
 	container:SetSize(15, 15)
 
+	local texture = container:CreateTexture()
+	texture:SetAllPoints(container)
+	texture:SetTexture(ns.config.mailIcon)
+	
 	style.addShadow(container)
 	style.applyBackgroundTo(container)
 
@@ -19,7 +24,19 @@ ns.features.styleMail = function(model)
 	local icon = model.mail.icon
 
 	button:SetAllPoints(container)
-	icon:SetAllPoints(container)
 	border:Hide()
+
+	icon:SetAllPoints(container)
+	icon:SetTexture(nil)
+
+	events.register("UPDATE_PENDING_MAIL", function()
+		
+		if HasNewMail() then
+			texture:SetVertexColor(1.0, 1.0, 1.0)
+		else
+			texture:SetVertexColor(0.5, 0.5, 0.5)
+		end
+
+	end)
 
 end

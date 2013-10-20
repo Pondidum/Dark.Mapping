e3d9e27bdab68aba2015b4dda25a9c571b4a7c55
andy <andy@stormbase.net>
2013-10-20 12:16:53 +0100

    styleMail: define map...

--- features/styleMail.lua
+++ features/styleMail.lua
@@ -6,6 +6,7 @@ local style = core.style
 ns.features.styleMail = function(model)
 
 	local mail = model.icons.mail
+	local map = model.map
 
 	mail:SetPoint("TOPRIGHT", map, "BOTTOMRIGHT", 0, -ns.config.spacing)
 	mail:SetSize(15, 15)

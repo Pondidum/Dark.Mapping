cd11eeb1f8c00ad18aa96ab675d3b4c4bb1846e1
andy <andy@stormbase.net>
2013-10-25 18:54:45 +0100

    buttonContainer: add layout, hard code first button

--- features/buttonContainer.lua
+++ features/buttonContainer.lua
@@ -1,7 +1,23 @@
 local addon, ns = ...
 
+local core = Dark.core
+local style = core.style
+local layout = core.layout
+
 ns.features.add(function(model)
 
-	-- button container
+	local container = CreateFrame("Frame", nil, UIParent)
+
+	container:SetPoint("LEFT", model.map, "LEFT", 0, 0)
+	container:SetPoint("TOPRIGHT", model.notificationContainer, "BOTTOMRIGHT", 0, -ns.config.spacing)
+	container:SetHeight(20)
+
+	style.addShadow(container)
+
+	layout.init(container, {})
+
+	container.add(QueueStatusMinimapButton)
+	QueueStatusMinimapButton:SetParent(container)
+
 
 end)
